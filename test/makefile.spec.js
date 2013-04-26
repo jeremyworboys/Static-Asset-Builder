1dfed127ca529263602f13b0b6bcd76b0e590110
Jeremy Worboys <jw@jeremyworboys.com>
2013-04-26 11:26:26 +1000

    Add image tests

--- test/makefile.spec.js
+++ test/makefile.spec.js
@@ -8,9 +8,15 @@ describe('Makefile', function() {
 
     describe('Styles', function() {});
 
-    describe('Images', function() {});
-
     describe('Fonts', function() {
+        it('should create the build directory', function(done) {
+            test('fonts', 'fonts', function(err, stdout) {
+                if (err) return done(err);
+                stdout.should.include('mkdir -p build/fonts');
+                done();
+            });
+        });
+
         it('should copy all fonts to the build directory', function(done) {
             test('fonts', 'fonts', function(err, stdout) {
                 if (err) return done(err);
@@ -27,6 +33,117 @@ describe('Makefile', function() {
                     if (err) return done(err);
                     stdout.should.include('Nothing to be done');
                     done();
+                });
+            });
+        });
+    });
+
+    describe('Images', function() {
+        it('should create the build directory', function(done) {
+            test('images-png', 'images', function(err, stdout) {
+                if (err) return done(err);
+                stdout.should.include('mkdir -p build/images');
+                done();
+            });
+        });
+
+        describe('PNG', function() {
+            it('should optimise images with pngnq', function(done) {
+                test('images-png', 'images', function(err, stdout) {
+                    if (err) return done(err);
+                    stdout.should.include('pngnq -s10 -f -d build/images -e .png lib/images/image-1.png');
+                    stdout.should.include('pngnq -s10 -f -d build/images -e .png lib/images/image-2.png');
+                    done();
+                });
+            });
+
+            it('should compress images with pngout', function(done) {
+                test('images-png', 'images', function(err, stdout) {
+                    if (err) return done(err);
+                    stdout.should.include('pngout build/images/image-1.png');
+                    stdout.should.include('pngout build/images/image-2.png');
+                    done();
+                });
+            });
+
+            it('should not rebuild up-to-date rules', function(done) {
+                test('images-png', 'images', function(err) {
+                    if (err) return done(err);
+                    test(false, 'images', function(err, stdout) {
+                        if (err) return done(err);
+                        stdout.should.include('Nothing to be done');
+                        done();
+                    });
+                });
+            });
+        });
+
+        describe('JPG', function() {
+            it('should compress images with jpegoptim', function(done) {
+                test('images-jpg', 'images', function(err, stdout) {
+                    if (err) return done(err);
+                    stdout.should.include('jpegoptim  -o -q -d build/images lib/images/image-1.jpg');
+                    stdout.should.include('jpegoptim  -o -q -d build/images lib/images/image-2.jpg');
+                    done();
+                });
+            });
+
+            it('should not rebuild up-to-date rules', function(done) {
+                test('images-jpg', 'images', function(err) {
+                    if (err) return done(err);
+                    test(false, 'images', function(err, stdout) {
+                        if (err) return done(err);
+                        stdout.should.include('Nothing to be done');
+                        done();
+                    });
+                });
+            });
+        });
+
+        describe('GIF', function() {
+            it('should copy all images to the build directory', function(done) {
+                test('images-gif', 'images', function(err, stdout) {
+                    if (err) return done(err);
+                    stdout.should.include('cp lib/images/image-1.gif build/images/image-1.gif');
+                    stdout.should.include('cp lib/images/image-2.gif build/images/image-2.gif');
+                    done();
+                });
+            });
+
+            it('should not rebuild up-to-date rules', function(done) {
+                test('images-gif', 'images', function(err) {
+                    if (err) return done(err);
+                    test(false, 'images', function(err, stdout) {
+                        if (err) return done(err);
+                        stdout.should.include('Nothing to be done');
+                        done();
+                    });
+                });
+            });
+        });
+
+        describe('All', function() {
+            it('should copy all images to the build directory', function(done) {
+                test('images-all', 'images', function(err, stdout) {
+                    if (err) return done(err);
+                    stdout.should.include('cp lib/images/image-1.gif build/images/image-1.gif');
+                    stdout.should.include('cp lib/images/image-2.gif build/images/image-2.gif');
+                    stdout.should.include('jpegoptim  -o -q -d build/images lib/images/image-1.jpg');
+                    stdout.should.include('jpegoptim  -o -q -d build/images lib/images/image-2.jpg');
+                    stdout.should.include('pngnq -s10 -f -d build/images -e .png lib/images/image-1.png');
+                    stdout.should.include('pngnq -s10 -f -d build/images -e .png lib/images/image-2.png');
+                    done();
+                });
+            });
+
+            it('should not rebuild up-to-date rules', function(done) {
+                test('images-all', 'images', function(err) {
+                    if (err) return done(err);
+                    test(false, 'images', function(err, stdout) {
+                        if (err) return done(err);
+                        stdout.should.include('Nothing to be done');
+                        done();
+                    });
                 });
             });
         });
